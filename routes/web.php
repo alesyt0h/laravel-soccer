f0fcac0ad2317f4c2c969fbf1409e7cdd0206740
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-23 10:53:08 +0100

    Added Show routes and POST/PUT/DELETE routes

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// TODO: Route Groups
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Create
+// Create Views
 Route::get('create/college');
 Route::get('create/club');
 Route::get('create/team');
 Route::get('create/match');
 
-// Update
+// Create Action
+Route::post('create/college');
+Route::post('create/club');
+Route::post('create/team');
+Route::post('create/match');
+
+// Show
+Route::get('show/college');
+Route::get('show/club');
+Route::get('show/team');
+Route::get('show/match');
+
+// Update Views
 Route::get('update/college/{id}', function ($id) {});
 Route::get('update/club/{id}', function ($id) {});
 Route::get('update/team/{id}', function ($id) {});
 Route::get('update/match/{id}', function ($id) {});
 
-// Delete
+// Update Action
+Route::put('update/college/{id}', function ($id) {});
+Route::put('update/club/{id}', function ($id) {});
+Route::put('update/team/{id}', function ($id) {});
+Route::put('update/match/{id}', function ($id) {});
+
+// Delete Views
 Route::get('delete/college/{id}', function ($id) {});
 Route::get('delete/club/{id}', function ($id) {});
 Route::get('delete/team/{id}', function ($id) {});
 Route::get('delete/match/{id}', function ($id) {});
+
+// Delete Action
+Route::delete('delete/college/{id}', function ($id) {});
+Route::delete('delete/club/{id}', function ($id) {});
+Route::delete('delete/team/{id}', function ($id) {});
+Route::delete('delete/match/{id}', function ($id) {});
