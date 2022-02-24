3287fd61bea1fbb6ca5dc4d6ea38f38235a50526
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-24 09:38:44 +0100

    Added a List component to reuse a table showing Clubs, Colleges..etc

---
+++ resources/views/components/listing.blade.php
@@ -0,0 +1,6 @@
+<ol>
+    <li>First record</li>
+    <li>Second record</li>
+    <li>Third record</li>
+    <li>Fourth record</li>
+</ol>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,23 @@
 
 @section('content')
     <h1>This is the homePage</h1>
+    <main style="display: grid; grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);"">
+        <div>
+            <span>Colleges</span>
+            <x-listing/>
+        </div>
+        <div>
+            <span>Clubs</span>
+            <x-listing/>
+        </div>
+        <div>
+            <span>Teams</span>
+            <x-listing/>
+        </div>
+        <div>
+            <span>Matches</span>
+            <x-listing/>
+        </div>
+    </main>
 @endsection
