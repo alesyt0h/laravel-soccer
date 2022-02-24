59af0bde2f840b4093fe844ea725ef10a502322f
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-24 12:52:28 +0100

    Added Update Forms

--- resources/views/college/update.blade.php
+++ resources/views/college/update.blade.php
@@ -4,5 +4,22 @@
 
 @section('content')
     <h1>This will be where you could edit a college</h1>
-    <h2>Form placeholder</h2>
+    <h2>College updation</h2>
+
+    <form action="" method="post">
+        @csrf
+        <input type="hidden" name="_method" value="PUT">
+
+        <label for="name">Name*:</label>
+        <input type="text" name="name"><br>
+
+        <label for="shield">Shield:</label>
+        <input type="text" name="shield"><br>
+
+        <label for="foundation">Foundation Date*:</label>
+        <input type="text" name="foundation"><br>
+
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+    </form>
 @endsection
