0cf631c555b17b86c853db05552aa9148691396b
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-24 12:38:59 +0100

    Added Creation Forms

--- resources/views/match/create.blade.php
+++ resources/views/match/create.blade.php
@@ -3,6 +3,58 @@
 @section('title', 'Create a new match')
 
 @section('content')
+    @if( Session::has('result') )
+        {{-- TODO Move this to a component --}}
+        <div class="alert alert-success alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('result') }}
+        </div>
+    @endif
+
     <h1>This is where matches are created</h1>
-    <h2>Form placeholder</h2>
+    <h2>Match creation</h2>
+
+    <form action="" method="post">
+        @csrf
+        <label for="name">Date*:</label>
+        <input type="text" name="date"><br>
+
+        <label for="local">Local Team:</label>
+        <select name="local_teams" id="">
+            <option value=""></option>
+            <option value="manchester">Manchester Utd.</option>
+            <option value="madrid">Madrid</option>
+            <option value="barcelona">FC Barcelona</option>
+        </select>
+        <br>
+
+        <label for="visitor">Visitor Team:</label>
+        <select name="visitor_teams" id="">
+            <option value=""></option>
+            <option value="manchester">Manchester Utd.</option>
+            <option value="madrid">Madrid</option>
+            <option value="barcelona">FC Barcelona</option>
+        </select>
+        <br>
+
+        <label for="status">Match Status:</label>
+        <select name="status" id="">
+            <option value="In Process" selected>In Process</option>
+            <option value="Played">Played</option>
+            <option value="Cancelled">Cancelled</option>
+        </select>
+        <br>
+
+        <label for="result">Match Result:</label>
+        <select name="result" id="">
+            <option value="Local">Local</option>
+            <option value="Visitor">Visitor</option>
+            <option value="Draw">Draw</option>
+            <option value="Not played yet" selected>Not played yet</option>
+        </select>
+        <br>
+
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+    </form>
 @endsection
--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -3,6 +3,42 @@
 @section('title', 'Create a new team')
 
 @section('content')
+    @if( Session::has('result') )
+        {{-- TODO Move this to a component --}}
+        <div class="alert alert-success alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('result') }}
+        </div>
+    @endif
+
     <h1>This is where teams are created</h1>
-    <h2>Form placeholder</h2>
+    <h2>Team Creation</h2>
+
+    <form action="" method="post">
+        @csrf
+        <label for="name">Name*:</label>
+        <input type="text" name="name"><br>
+
+        <label for="foundation_date">Foundation Date*:</label>
+        <input type="text" name="foundation_date"><br>
+
+        <label for="shield">Shield:</label>
+        <input type="text" name="shield"><br>
+
+        <label for="owner">Owner:</label>
+        <input type="radio" name="owner_type" id="" value="college">College
+        <input type="radio" name="owner_type" id="" value="club">Club
+        <br>
+        {{-- Show this conditionally depending on the checkbox --}}
+        <select name="owner" id="">
+            <option value=""></option>
+            <option value="1">Oxford</option>
+            <option value="2">Harvard</option>
+            <option value="3">Club del Barcelona</option>
+        </select>
+        <br>
+
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+    </form>
 @endsection
