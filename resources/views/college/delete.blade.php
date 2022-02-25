a20c186ff5feee7bc0387d82cb44a5ac477546a9
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-25 22:03:14 +0100

    Added Delete Forms

--- resources/views/college/delete.blade.php
+++ resources/views/college/delete.blade.php
@@ -4,5 +4,13 @@
 
 @section('content')
     <h1>This will be for delete a college</h1>
-    <h2>Confirmation of deletion placeholder</h2>
+    <h2>College deletion</h2>
+    <form action="" method="post">
+        @csrf
+        <input type="hidden" name="_method" value="DELETE">
+        <label for="confirm">Are you sure you want to delete {{$college}} ?</label>
+        <br>
+        <button type="submit" name="id" value="{{$college}}">Yes</button>
+        <a href="{{url()->previous()}}">No</a>
+    </form>
 @endsection
