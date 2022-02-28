68486d7eda83eb4c128cd2f5508dd8b5f41ecec4
Alex Ortigosa <alexortigosa179@gmail.com>
2022-02-28 20:15:07 +0100

    Added migrations for Club, Colleges, Teams & Matches Tables

---
+++ database/migrations/2022_02_28_123336_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->foreignId('college_owner')->nullable()->references('id')->on('colleges');
+            $table->foreignId('club_owner')->nullable()->references('id')->on('clubs');
+            $table->string('shield', 255)->nullable();
+            $table->date('foundation_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
---
+++ database/migrations/2022_02_28_123346_create_matches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('visitor')->references('id')->on('teams');
+            $table->foreignId('local')->references('id')->on('teams');
+            $table->dateTimeTz('match_date');
+            $table->enum('result', ['local', 'visitor', 'draw', 'not played yet'])->nullable();
+            $table->enum('status', ['played', 'canceled', 'in progress']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+};
