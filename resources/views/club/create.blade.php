5bcfe7d1cb6926c236d0eca59afe2d42afde5d5f
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-03 12:31:07 +0100

    Displays the fields errors in Blade for Club

--- resources/views/club/create.blade.php
+++ resources/views/club/create.blade.php
@@ -16,14 +16,24 @@
 
     <form action="" method="post">
         @csrf
+
         <label for="name">Name*:</label>
         <input type="text" name="name"><br>
+        @error('name')
+            <small>*{{$message}}</small>
+        @enderror
 
         <label for="shield">Shield:</label>
         <input type="text" name="shield"><br>
+        @error('shield')
+            <small>*{{$message}}</small>
+        @enderror
 
         <label for="foundation">Foundation Date*:</label>
         <input type="text" name="foundation"><br>
+        @error('foundation')
+            <small>*{{$message}}</small>
+        @enderror
 
         <button type="submit">Submit</button>
         <button type="reset">Reset</button>
