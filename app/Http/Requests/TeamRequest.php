83944eace2f8efa1dbc7932982cfc3c2ca0253be
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-04 18:02:58 +0100

    Added a TeamRequest

---
+++ app/Http/Requests/TeamRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3',
+            'shield' => 'max:255',
+            'foundation' => 'required|date',
+            'owner_type' => 'required|in:college,club',
+            'owner' => 'required|numeric'
+        ];
+    }
+}
