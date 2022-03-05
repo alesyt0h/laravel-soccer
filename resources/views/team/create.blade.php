1d097f8d603d2cfe0d3dc36382e38e516a192b21
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-05 18:49:42 +0100

    Modified Team Blade Templates to work with CRUD and result msg

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -44,9 +44,15 @@
         {{-- Show this conditionally depending on the checkbox --}}
         <select name="owner" id="">
             <option value=""></option>
-            <option value="1">Oxford</option>
-            <option value="2">Harvard</option>
-            <option value="3">Club del Barcelona</option>
+            @foreach ($colleges as $college)
+                <option value="{{$college->id}}">{{$college->name}}</option>
+            @endforeach
+        </select>
+        <select name="" id="" style="display:none">
+            <option value=""></option>
+            @foreach ($clubs as $club)
+                <option value="{{$club->id}}">{{$club->name}}</option>
+            @endforeach
         </select>
         @error('owner')
             <small>*{{$message}}</small>
--- resources/views/team/delete.blade.php
+++ resources/views/team/delete.blade.php
@@ -5,8 +5,10 @@
 @section('content')
     <h1>This will be for delete a team</h1>
     <h2>Team deletion</h2>
-    <form action="" method="post">
+    <form action="{{route('team.destroy', $team)}}" method="post">
         @csrf
+        @method('DELETE')
+
         <input type="hidden" name="_method" value="DELETE">
         <label for="confirm">Are you sure you want to delete {{$team}} ?</label>
         <br>
