0f933c35a7c554cb0c29349142f6873ba97fee8a
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-08 09:49:38 +0100

    Installed Tailwind

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body style="background: rgb(212, 212, 212)">
     <x-sidebar/>
