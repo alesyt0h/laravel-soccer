1ca37f9b4fdb19c896e30c367a9bb2f7cef723f9
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-08 09:13:48 +0100

    Added the session block to display a result msg in Team/delete

--- resources/views/team/delete.blade.php
+++ resources/views/team/delete.blade.php
@@ -3,6 +3,13 @@
 @section('title', 'Delete ' . $team)
 
 @section('content')
+    @if( Session::has('result') )
+        {{-- TODO Move this to a component --}}
+        <div class="alert alert-success alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('result') }}
+        </div>
+    @endif
     <h1>This will be for delete a team</h1>
     <h2>Team deletion</h2>
     <form action="{{route('team.destroy', $team)}}" method="post">
