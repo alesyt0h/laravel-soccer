0fa1424e9becab9d2ea661e7cd12d8bbca34e34b
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-10 19:48:59 +0100

    Added a MIME Validator for Shield

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
@@ -24,6 +25,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('mimeurl',
+            function($attribute, $value, $parameters, $validator) {
+                $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                $mimetype = $finfo->buffer(file_get_contents($value));
+                return in_array($mimetype, $parameters);
+            }, 'The :attribute must be a valid image URL.'
+        );
+
+        Validator::replacer('mimeurl',
+            function($message, $attribute, $rule, $parameters) {
+                return str_replace(':values', implode(', ', $parameters), $message);
+            }
+        );
     }
 }
