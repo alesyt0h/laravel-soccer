529dfdffae5f9e06ca8336152efea2379ac71fac
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-14 11:47:52 +0100

    Fixed red border for Alert Component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 @if( Session::has('result') )
-    <div class="{{ (Session::get('success')) ? 'bg-green-200 text-green-700 border-green-800/70' : 'bg-red-200 text-red-700 border-red-800/70' }} p-4 rounded-lg border border-red-800/70 mb-2 m-auto w-fit">
+    <div class="{{ (Session::get('success')) ? 'bg-green-200 text-green-700 border-green-800/70' : 'bg-red-200 text-red-700 border-red-800/70' }} p-4 rounded-lg border mb-2 m-auto w-fit">
         {{ Session::get('result') }}
     </div>
 @endif
