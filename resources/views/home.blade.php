bc1acccd8c644b9f6c19612f1f7863f62117c97e
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-14 22:03:46 +0100

    Main Layout and Home responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div>
         <h1 class="text-2xl font-semibold text-center mb-2">Soccer League</h1>
         <x-alert/>
-        <main class="scale-90 grid grid-cols-2 grid-rows-2">
+        <main class="scale-90 grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:grid-rows-2">
             <div class="mb-8 mt-8">
                 <x-listing :entity="$colleges" type="college" from="home"/>
             </div>
