7575af4b1fdea45199541f036aee0168779380f8
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-17 00:25:37 +0100

    Modified Main & Home Layouts to be able to adapt to the admin dashboard template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title'){{(request()->routeIs('home')) ? config('app.name') : ' | ' . config('app.name')}}</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 </head>
-<body class="overflow-x-hidden !bg-cover !bg-fixed" style="background: url({{ asset('images/football-bg.jpg') }})">
-    @if (request()->is([
-        'login',
-        'register',
-        'forgot-password',
-        'reset-password/*',
-        'confirm-password',
-        'verify-email/*'
-    ]))
-        @php
-            $noMargin = true;
-        @endphp
-    @else
-        <x-sidebar/>
-    @endif
-    <div class="p-14 sm:p-10 bg-gray-500/40 flex min-h-screen max-h-max justify-center p-10 {{($noMargin ?? null) ? '' : 'sm:ml-[300px]'}}">
-        <div class="relative bg-slate-200/80 m-auto p-10 self-center rounded-2xl shadow-2xl flex justify-center border border-slate-500
-             {{ (request()->is(['club', 'college', 'team', 'match'])) ? 'w-full lg:w-screen' : ''}}
-             {{ (request()->routeIs('home')) ? 'w-full pb-0' : ''}}">
-            @yield('content')
+<body>
+    <div class="mx-auto bg-grey-lightest">
+        <div class="min-h-screen flex flex-col">
+            <x-header/>
+            <div class="flex flex-1">
+                <x-sidebar/>
+                <main class="bg-white-300 flex-1 overflow-hidden self-center
+                {{ (request()->is(['login', 'register', 'forgot-password', 'reset-password/*', 'confirm-password', 'verify-email/*', 'verify-email'])) ? 'p-16' : 'p-3'}}">
+                    <div class="flex flex-col">
+                        <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 justify-center">
+                            @yield('content')
+                        </div>
+                    </div>
+                </main>
+            </div>
         </div>
     </div>
 </body>
