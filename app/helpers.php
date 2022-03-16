fa7cc06abeb7a87e5b333715d0800c9dd021e9a6
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-16 11:36:46 +0100

    Added a isAdmin function and changed isSameUser to isSameUserOrAdmin

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,14 @@
 <?php
 
 if (! function_exists('isSameUser')) {
-    function isSameUser($entity){
-        return $entity->created_by === auth()->user()->id;
+    function isSameUserOrAdmin($entity){
+        return $entity->created_by === auth()->user()->id || isAdmin();
+    }
+}
+
+if (! function_exists('isAdmin')) {
+    function isAdmin(){
+        return auth()->user()->role === 'admin';
     }
 }
 
